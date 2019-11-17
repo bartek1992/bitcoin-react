bfab234b6405bec3d2229455f5073b6df1627db7
Bartosz Tąpała <bartosztapala@gmail.com>
2019-11-17 16:27:28 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Crypto from './Crypto'
+
+function App() {
+  return (
+    <div className="App">
+      <Crypto />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CryptoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const CryptoList = props => {
+    console.log(props.cryptoArray);
+
+    let cryptoList = props.cryptoArray.map(crypto => {
+        return <li key={crypto.currency}>Last rate: <span className={crypto.class}>{crypto.last}</span> {crypto.currency} {crypto.symbol}</li>;
+    });
+
+    return (
+        <ul className='crypto-list'>
+            {cryptoList}
+        </ul>
+    );
+}
+
+export default CryptoList;
