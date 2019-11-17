609de78e3977de97bcdfe0e7fbcfe9d05f20795c
Bartosz Tąpała <bartosztapala@gmail.com>
2019-11-17 16:40:36 +0100

    Updates

---
+++ precache-manifest.ed156f2276a46dd736b0908ea9f45e06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb995d1f7795d99c2e438346a88e041",
+    "url": "/bitcoin-react/index.html"
+  },
+  {
+    "revision": "2017c670f1aeee3bb830",
+    "url": "/bitcoin-react/static/css/main.7e318520.chunk.css"
+  },
+  {
+    "revision": "206c021eb127509de74f",
+    "url": "/bitcoin-react/static/js/2.0f5dd65a.chunk.js"
+  },
+  {
+    "revision": "2017c670f1aeee3bb830",
+    "url": "/bitcoin-react/static/js/main.6d0bb659.chunk.js"
+  },
+  {
+    "revision": "6254211b5c307dcd4268",
+    "url": "/bitcoin-react/static/js/runtime-main.b9619e0c.js"
+  }
+]);
